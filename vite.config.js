47fae0cccede03f18149af8e2c41d8070102888f
Arbaj78 <khanarbaj01985@gmail.com>
2025-07-22 15:25:00 +0530

    fix white

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // ✅ added for correct asset loading on GitHub Pages + custom domain
+  base: './', // ✅ added for correct asset loading on GitHub Pages + custom domain
   plugins: [react()],
   server: {
     host: true,
